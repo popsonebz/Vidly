3cd0fff35c08a30683ab6f7880ed85922ae71b65
popsonebz <popoolaebenezer@gmail.com>
2018-03-02 14:54:59 +0200

    retrieving data from database using controller

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Collections.Generic;
+using Vidly.Models;
+using Vidly.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context; //we need to declare this because it connects us to the database
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context; //its initialized here
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ViewResult Index() //vieresult is a subtype of actionresult
+        {
+            //var customers = _context.Customers; // remember, we defined Customers context in ApplicationDbContext.cs
+            //note: this query will not excute until you use it in the view
+
+            //to force it to execute the sql here, we use
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return NotFound();  
+            }
+            return View(customer);
+        }
+    }
+    
+}
