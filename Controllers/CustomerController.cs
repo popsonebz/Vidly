8367cec0575d4395c4199a3429313186451277a2
popsonebz <popoolaebenezer@gmail.com>
2018-03-03 14:01:22 +0200

    editing forms

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -59,6 +59,21 @@ namespace Vidly.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index", "Customers");
         }
+
+        
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();  
+            }
+            var viewModel = new CustomerViewModel
+            {Customer = customer,
+             MembershipTypes = _context.MembershipType.ToList()};
+            
+            return View("New", viewModel);
+        }
     }
     
 }
